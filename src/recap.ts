38b842bb80b2300ea2af269f6f8d39d4e091b7fd
Gav Pro <GavPro@example.com>
2022-11-07 01:56:44 -0500

    Modification to 'recap.ts' file.

--- src/recap.ts
+++ src/recap.ts
@@ -1,5 +1,5 @@
-const myName = 'Pepe';
-const myAge = 12;
+const myName = 'Pepe 3-s';
+const myAge = 18;
 const suma = (a: number, b: number) => {
   return a + b;
 };
@@ -13,5 +13,5 @@ class Persona {
   }
 }
 
-const pepe = new Persona(15, 'pepe');
+const pepe = new Persona(15, 'pepe 3-s');
 pepe.getSummary();
